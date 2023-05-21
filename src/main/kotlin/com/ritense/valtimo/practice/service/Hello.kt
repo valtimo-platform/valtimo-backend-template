ea452f2170944037f43f87fa4dc228ce2cb3c390
Erik de Vries <erik.de.vries@ritense.com>
2023-05-21 11:22:25 +0200

    practice process

---
+++ src/main/kotlin/com/ritense/valtimo/practice/service/Hello.kt
@@ -0,0 +1,15 @@
+package com.ritense.valtimo.practice.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class Hello {
+
+    fun hello(input: String) {
+        println(input)
+    }
+
+    fun error(input: String) {
+        println(input)
+    }
+}
