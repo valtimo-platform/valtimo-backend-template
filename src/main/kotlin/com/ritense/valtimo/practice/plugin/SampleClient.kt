bfb95454d8aabef49dc600cd600baf3daa60cbbf
Erik de Vries <erik.de.vries@ritense.com>
2023-05-21 12:17:44 +0200

    sample plugin

---
+++ src/main/kotlin/com/ritense/valtimo/practice/plugin/SampleClient.kt
@@ -0,0 +1,11 @@
+package com.ritense.valtimo.practice.plugin
+
+import org.springframework.stereotype.Component
+
+@Component
+class SampleClient {
+
+    fun hello() {
+        println("PLUGIN HELLO")
+    }
+}
---
+++ src/main/kotlin/com/ritense/valtimo/practice/plugin/SamplePluginDto.kt
@@ -0,0 +1,7 @@
+package com.ritense.valtimo.practice.plugin
+
+data class SamplePluginDto(
+    val url: String,
+    val username: String,
+    val password: String
+)
