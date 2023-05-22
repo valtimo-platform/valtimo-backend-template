654ca15528fc5db565ba7df784c2c8895135e3c9
Erik de Vries <erik.de.vries@ritense.com>
2023-05-22 09:07:34 +0200

    sample plugin actions -WIP

--- src/main/kotlin/com/ritense/valtimo/practice/plugin/SamplePlugin.kt
+++ src/main/kotlin/com/ritense/valtimo/practice/plugin/SamplePlugin.kt
@@ -2,6 +2,7 @@ package com.ritense.valtimo.practice.plugin
 
 import com.ritense.plugin.annotation.Plugin
 import com.ritense.plugin.annotation.PluginAction
+import com.ritense.plugin.annotation.PluginActionProperty
 import com.ritense.plugin.annotation.PluginProperty
 import com.ritense.plugin.domain.ActivityType
 import org.camunda.bpm.engine.delegate.DelegateExecution
@@ -25,13 +26,16 @@ class SamplePlugin(
     lateinit var password: String
 
     @PluginAction(
-        key = "sample",
+        key = "sample-action",
         title = "Sample action",
         description = "Sample plugin action",
-        activityTypes = [ActivityType.SERVICE_TASK]
+        activityTypes = [ActivityType.SERVICE_TASK_START]
     )
-    fun samplePrinter(execution: DelegateExecution) {
-            sampleClient.hello()
+    fun samplePrinter(
+        execution: DelegateExecution,
+        @PluginActionProperty sampleString: String
+    ) {
+        sampleClient.hello()
     }
 
 }
