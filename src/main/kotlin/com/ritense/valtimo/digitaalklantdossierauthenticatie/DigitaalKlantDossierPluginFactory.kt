96e85d649e0f7e1dc05c57ccdacf047b3cd6f929
Thomas Minke <thomas.minke@ritense.com>
2023-06-28 11:29:17 +0200

    Fix ktlint.

--- src/main/kotlin/com/ritense/valtimo/digitaalklantdossierauthenticatie/DigitaalKlantDossierPluginFactory.kt
+++ src/main/kotlin/com/ritense/valtimo/digitaalklantdossierauthenticatie/DigitaalKlantDossierPluginFactory.kt
@@ -2,12 +2,9 @@ package com.ritense.valtimo.digitaalklantdossierauthenticatie
 
 import com.ritense.plugin.PluginFactory
 import com.ritense.plugin.service.PluginService
-import com.ritense.valtimo.digitaalklantdossierauthenticatie.DigitaalKlantDossierPlugin
-
 
 class DigitaalKlantDossierPluginFactory(
     pluginService: PluginService
 ): PluginFactory<DigitaalKlantDossierPlugin>(pluginService) {
-
-    override fun create(): DigitaalKlantDossierPlugin = DigitaalKlantDossierPlugin()
+    override fun create() : DigitaalKlantDossierPlugin = DigitaalKlantDossierPlugin()
 }
--- src/main/kotlin/com/ritense/valtimo/practice/plugin/SamplePluginFactory.kt
+++ src/main/kotlin/com/ritense/valtimo/practice/plugin/SamplePluginFactory.kt
@@ -2,8 +2,6 @@ package com.ritense.valtimo.practice.plugin
 
 import com.ritense.plugin.PluginFactory
 import com.ritense.plugin.service.PluginService
-import org.springframework.stereotype.Component
-
 
 class SamplePluginFactory(
     pluginService: PluginService,
