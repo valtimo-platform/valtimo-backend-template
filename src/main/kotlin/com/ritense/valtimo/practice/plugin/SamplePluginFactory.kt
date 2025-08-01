5552d22a6b9667521b02fe1f4f1eba673df22e13
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2025-08-01 13:26:36 +0200

    Upgrade to 12.15.1

--- src/main/kotlin/com/ritense/valtimo/practice/plugin/SamplePluginFactory.kt
+++ src/main/kotlin/com/ritense/valtimo/practice/plugin/SamplePluginFactory.kt
@@ -7,7 +7,5 @@ class SamplePluginFactory(
     pluginService: PluginService,
     val sampleClient: SampleClient,
 ) : PluginFactory<SamplePlugin>(pluginService) {
-    override fun create(): SamplePlugin {
-        return SamplePlugin(sampleClient)
-    }
+    override fun create(): SamplePlugin = SamplePlugin(sampleClient)
 }
